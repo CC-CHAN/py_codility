90cbc2a0624c7d02075a934898aa394e1f0bc3b7
CC-CHAN <ccchanigel@gmail.com>
2018-12-16 23:19:12 +0800

    Create 1.1 BinaryGap.py

---
+++ 1.1 BinaryGap.py
@@ -0,0 +1,9 @@
+def solution(N):
+    num = str(bin(N))
+    myList = num.split('1')
+    myList = myList[1:]
+    if(not num.endswith('1')):
+        myList = myList[:-1]
+    if(len(myList) < 1):
+        return 0
+    return len(max(myList, key=len))
