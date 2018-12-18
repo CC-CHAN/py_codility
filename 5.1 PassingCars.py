987b4cd2772bc9316b0b4eada6b62fd8419c637c
CC-CHAN <ccchanigel@gmail.com>
2018-12-18 21:34:39 +0800

    Create 5.1 PassingCars.py

---
+++ 5.1 PassingCars.py
@@ -0,0 +1,11 @@
+def solution(A):
+    N = len(A)
+    zeroCount = 0
+    l = [0] * N
+    for x in range(1, N):
+        if(A[x-1]==0):
+            zeroCount +=1
+        if(A[x]==1):
+            l[x] += zeroCount
+    result = sum(l)
+    return result if result < 1000000000 else -1
